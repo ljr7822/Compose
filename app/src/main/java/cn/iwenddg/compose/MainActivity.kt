e04fb9074ce81b9d13259a6e7e0769578d24893b
iwenLjr <1816807688@qq.com>
2021-05-31 19:00:32 +0800

    Initial commit Compose

---
+++ app/src/main/java/cn/iwenddg/compose/MainActivity.kt
@@ -0,0 +1,15 @@
+package cn.iwenddg.compose
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Text("你好 Jet Compose")
+        }
+    }
+}
